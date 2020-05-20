4ed8a4c7c6e677fed3afc40d7e727130295ee9bc
Michał Świderski <swiderskimichal@outlook.com>
2020-05-20 23:46:02 +0200

    initial commit

---
+++ src/views/Patients.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <v-container>
+            <h1 class="title">Patient List</h1>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Patients"
+    }
+</script>
+
+<style scoped>
+
+</style>
