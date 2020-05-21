deb2e8448e991cbb135a8ccf6ed5ccf38e31f0bf
Michał Świderski <swiderskimichal@outlook.com>
2020-05-21 21:03:05 +0200

    add pagination and filtering

--- src/mixins/FHIRMixin.js
+++ src/mixins/FHIRMixin.js
@@ -5,9 +5,18 @@ const URL = `http://${e.VUE_APP_FHIR_HOST}:${e.VUE_APP_FHIR_PORT}/${e.VUE_APP_FH
 
 export default {
     methods : {
-        async fhir_getAllPatients() {
-            const {data} = await axios.get(`${URL}/Patient`);
-            return data.entry.map(entry => entry.resource)
+        async fhir_getAllPatients(options = {}) {
+            console.log(options)
+            const {count, offset, filter} = options;
+            const paginationString = `_count=${count}&_getpagesoffset=${offset}`;
+
+            const filterString = filter.length ? `name=${filter}` : ''
+
+            const {data} = await axios.get(`${URL}/Patient?${paginationString}&${filterString}`);
+            return {
+                total : data.total,
+                result : data.total > 0 ? data.entry.map(entry => entry.resource) : []
+            }
         },
 
         async fhir_getPatient(id) {
